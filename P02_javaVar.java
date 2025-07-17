919b17cb4ed9ad0aefe4d2986a20789360e305f6
DhruviTailor1337 <dhruvitailor47@gmail.com>
2025-07-17 11:00:06 +0530

    staged changes

--- P02_javaVar.java
+++ P02_javaVar.java
@@ -1,22 +1,22 @@
 public class P02_javaVar {
     public static void main(String args[]){
         int age=18;
-        int year=2025;
-        int quantity=5;
+        //int year=2025;
+        //int quantity=5;
 
-        double price=1200.89;
+        //double price=1200.89;
         double spi=9.68;
-        double temp=37.86;
+        //double temp=37.86;
 
-        char latter='D';
+        //char latter='D';
         char grade='A';
-        char currency='$';
+        //char currency='$';
 
         boolean is_student=true;
-        boolean is_employee=false;
+        //boolean is_employee=false;
 
         String s1 ="Dhruvi";
-        String s2 ="Learning Java";
+        //String s2 ="Learning Java";
 
         //printing some variables 
         System.out.println("name:"+s1+"\nAge:"+age+"\nspi:"+spi+"\ngrade:"+grade+"\nIs student:"+is_student);
