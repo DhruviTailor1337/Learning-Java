6a9e207ce7aef0e7ecd18813bf6deaa533c96cf3
DhruviTailor1337 <dhruvitailor47@gmail.com>
2025-08-24 05:39:49 +0530

    Unary operator

---
+++ P04_operator1.java
@@ -0,0 +1,28 @@
+//unary operator
+public class P04_operator1 {
+    public static void main(String[] args) {
+        
+        int x = 10;
+        //post increment
+        System.out.println("value of x before post increment :"+x);
+        System.out.println(x++);
+        System.out.println("value of x after post increment :"+x);
+        System.out.println("\n");
+
+        //pre increment
+        System.out.println("value of x before pre increment:"+x);
+        System.out.println(++x);
+        System.out.println("value of x after pre increment:"+x);
+        System.out.println("\n");
+
+        //post decrement
+        System.out.println("value of x:"+x);
+        System.out.println(x--);
+        System.out.println("value of x after pre decrement:"+x);
+        System.out.println("\n");
+
+        System.out.println("value of x:"+x);
+        System.out.println(--x);
+        System.out.println("value of x after post decrement:"+x);
+    }
+}
