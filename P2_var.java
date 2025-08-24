044aa05629f593c189454960109d188214c88194
Dhruvi Tailor <dhruvitailor47@gmail.com>
2025-08-24 17:10:48 +0530

    variables

--- P2_var.java
+++ P2_var.java
@@ -1,3 +1,13 @@
 public class P2_var {
-    
+    public static void main(String[] args) {
+        int id = 123456;
+        String name = "Dhruvi Tailor";
+        char grade = 'A';
+        boolean is_student = true;
+
+        System.out.println("Student id:"+id);
+        System.out.println("Student name:"+name);
+        System.out.println("Student Grade:"+grade);
+        System.out.println("is student enrolled?:"+is_student);
+    }
 }
