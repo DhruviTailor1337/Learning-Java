606f9a0686b5820b3404b4486b19ffe4e3c3dac6
DhruviTailor1337 <dhruvitailor47@gmail.com>
2025-08-24 05:57:13 +0530

    Shift operator

---
+++ P05_operator3.java
@@ -0,0 +1,13 @@
+//shift operator
+public class P05_operator3 {
+    public static void main(String[] args) {
+
+        //left shift
+        System.out.println(10<<2);
+        System.out.println(10<<3);
+
+        //right shift
+        System.out.println(10>>2);
+        System.out.println(10>>3);
+    }
+}
