86b263d4d279eb88873892193a9a9c16de70b026
DhruviTailor1337 <dhruvitailor47@gmail.com>
2025-08-24 06:10:48 +0530

    Bitwise operator

---
+++ P05_operator5.java
@@ -0,0 +1,18 @@
+public class P05_operator5 {
+    public static void main(String[] args) {
+        int a = 2;
+        int b = 3;
+
+        //biwise and
+        System.out.println("a&b = "+(a&b));
+
+        //bitwise or
+        System.out.println("a|b = "+(a|b));
+
+        //biwise xor
+        System.out.println("a^b = "+(a^b));
+
+        //bitwise  not
+        System.out.println("~a = "+(~a));  //gives complement
+    }
+}
