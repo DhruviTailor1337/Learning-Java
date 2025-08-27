1b0b502c69e91d52739e61ccc8c8ef5ee69da3d7
DhruviTailor1337 <dhruvitailor47@gmail.com>
2025-08-27 08:59:50 +0530

    student details

---
+++ P06_basic_info.java
@@ -0,0 +1,15 @@
+public class P06_basic_info {
+    public static void main(String[] args) {
+        int id = 123456;
+        String name = "Dhruvi Tailor";
+        String dept = "Int.Msc.IT";
+        int age = 18;
+        float spi = 9.64f;
+
+        System.out.println("Student's id: "+id);
+        System.out.println("Student's name: "+name);
+        System.out.println("Student's age: "+age);
+        System.out.println("Student's department: "+dept);
+        System.out.println("Student's spi in second sem: "+spi);
+    }
+}
