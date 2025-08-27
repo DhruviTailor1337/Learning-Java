70e8d3d8cf66adf3605889b17c5c0229738c62ac
DhruviTailor1337 <dhruvitailor47@gmail.com>
2025-08-27 09:52:31 +0530

    passing marks

---
+++ P08_simple_if.java
@@ -0,0 +1,15 @@
+/*
+  if(condition){
+       statement block;
+  }
+ */
+
+public class P08_simple_if {
+
+    public static void main(String[] args) {
+        int marks = 75;
+        if (marks >= 75){
+            System.out.println("you are pass.");
+        }
+    }
+}
