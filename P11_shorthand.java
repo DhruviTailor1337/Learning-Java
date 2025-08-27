3af1ee4612946af131e65ceb3beaef06fb836645
DhruviTailor1337 <dhruvitailor47@gmail.com>
2025-08-27 10:12:21 +0530

    comparision of number

---
+++ P11_shorthand.java
@@ -0,0 +1,11 @@
+
+// There is also a short-hand if else, which is known as the ternary operator because it consists of three operands.
+
+// It can be used to replace multiple lines of code with a single line, and is most often used to replace simple if else statements
+// variable = (condition) ? expressionTrue :  expressionFalse;
+public class P11_shorthand {
+    public static void main(String[] args) {
+        String result = (13>10) ? "13 is greater" : "10 is greater" ;
+        System.out.println(result);
+    }
+}
