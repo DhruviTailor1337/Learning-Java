92587927c0ecbd3bc025091aed369feda98cec8c
DhruviTailor1337 <dhruvitailor47@gmail.com>
2025-08-27 09:10:11 +0530

    simple nerrowing.java

---
+++ P07_narrowing.java
@@ -0,0 +1,9 @@
+public class P07_narrowing {
+    public static void main(String[] args) {
+        double a = 9.64d;
+        int b = (int)a;     // Manual casting: double to int
+        System.out.println(a);
+        System.out.println(b);
+
+    }
+}
